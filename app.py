3757e3c52375b5d1ca3059aadf2fdb1793e5294b
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-06-10 06:37:40 +0000

    updating

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Planetary Insight Engine",
-    page_icon="🪐",
+    page_icon="assets/icons/icon.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
