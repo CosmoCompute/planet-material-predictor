fbbc1486556b44d7054c035df5ca2c54616716d6
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-06-10 06:55:33 +0000

    updating

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 from components import app_sidebar
 
-app_sidebar.create_sidebar()
-
 st.set_page_config(
     page_title="Planetary Insight Engine",
     page_icon="assets/icons/icon.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
+
+app_sidebar.create_sidebar()
+
+
 
