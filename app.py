128a13ed55ad4b7aef5d3051d2aa0298ca003394
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-06-10 06:26:57 +0000

    testing

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Planet Insight Engine",
+    page_title="Planetary Insight Engine",
     page_icon="🪐",
     layout="wide",
     initial_sidebar_state="expanded"
