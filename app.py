b7f1826d9552a12b5499a02f3f734dcfe67f602a
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-06-11 04:14:51 +0000

    solve

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from components import app_sidebar
-from components import lcoal_def
+from components import local_def
 
 
 st.set_page_config(
@@ -10,7 +10,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-lcoal_def.load_css("assets/style.css")
+local_def.load_css("assets/style.css")
 
 app_sidebar.create_sidebar()
 
