ced27fd44aa73a85aeb877b45d79d018aa136a6d
Arya Singh <aryasinghoffc3@gmail.com>
2025-06-15 13:30:43 +0530

    Update notfoundpage.py

--- components/notfoundpage.py
+++ components/notfoundpage.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
+# Define a function called 'notfound' to display a 404 error message
 def notfound():
+    # Display a subheader text indicating "404 NOT FOUND" on the Streamlit app
     st.subheader("404 NOT FOUND")
