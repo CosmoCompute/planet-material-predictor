1158bb657033c5268f7b05d4d36e0ffaa461cef1
Swarnabha Halder <swarnabha_202200248@smit.smu.edu.in>
2025-07-29 23:59:41 +0530

    Update about_page.py

--- components/about_page.py
+++ components/about_page.py
@@ -34,7 +34,7 @@ def about_us():
     # Mix of GitHub avatar (URL) and local image path
     profile = [
         "https://avatars.githubusercontent.com/u/143516210?v=4",  # Remote image
-        "assets/team/DPP40269.jpg"  # Local image
+        "assets/team/1000131485.jpg"  # Local image
     ]
 
     st.markdown("""<br>""", unsafe_allow_html=True)
